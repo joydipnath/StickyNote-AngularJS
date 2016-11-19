b4670cac719f4bcd84b8ed339a3d0ac09361a1a0
joydipnath <joydipsworld@gmail.com>
2016-11-19 17:06:10 +0530

    app.js has been updated and editor.html has been updated to edit view remove

--- app/app.js
+++ app/app.js
@@ -1,12 +1,29 @@
 var app = angular.module('plunker', ['ngStorage']);
 app.controller("editor", function($scope, $localStorage) {
-            $scope.editorText = $localStorage.message;
-            $scope.save = function(value) {
-                $localStorage.message = value;
-            }
+			$scope.notes = [];	
+			for(item in localStorage) {
+				var item1 = JSON.parse( localStorage[item] );
+				$scope.notes.push(item1);
+				
+			}	
+			$scope.save   = function(value) {	
+							//var showStored = document.getElementById('showStored');
+							var totalItem  = localStorage.length;
+							var key   = "StickyNote_Joydip-" + totalItem ;
+							var createData = {
+								id    : localStorage.length,
+								data  : value
+							
+							};
+					localStorage.setItem( key, JSON.stringify(createData) );
+					totalItem = localStorage.length;	
+					$scope.notes.push(createData);
+					
+				}
 
-            $scope.load = function() {
-                $scope.editorText = $localStorage.message;
+            $scope.remove = function(index,note) {
+				//alert(note.id);
+				localStorage.removeItem("StickyNote_Joydip-"+note.id);
+				$scope.notes.splice(index,1);
             }
-
-        });
+});
